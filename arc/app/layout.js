065ff9135330d5f4451558310905ae0c90f0f8e3
Matias Quenardelle <matiasquenardelle@gmail.com>
2025-04-23 12:17:41 +0200

    Updated layout.js

--- arc/app/layout.js
+++ arc/app/layout.js
@@ -4,14 +4,14 @@ import LanguageSwitcher from "@/components/LanguageSwitcher"
 export const metadata = {
   title: "Arcoiris de Amor",
   description: "Web de Vanina Starkoff",
-  icons: {
-    icon: "/favicon.ico?v=2",
-  },
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico?v=2" />
+      </head>
       <body>{children}</body>
     </html>
   )
